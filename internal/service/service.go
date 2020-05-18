2ff6cd80977af4e3835443dc138d530ce1a8cd44
Raja Shankar Kolluru <raja.shankar@meratransport.com>
2020-05-18 10:44:20 +0530

    minor tweaks for register

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,20 +6,21 @@ import (
 	"strings"
 )
 
-type stringdemo struct{}
+type stringdemo struct{
+}
 
 func MakeStringdemoService() stringdemo {
 	return stringdemo{}
 }
 
-func (stringdemo) Uppercase(ctx context.Context, ucr *api.UppercaseRequest) (api.UppercaseResponse, error) {
+func (stringdemo) Uppercase(_ context.Context, ucr *api.UppercaseRequest) (api.UppercaseResponse, error) {
 	return api.UppercaseResponse{V: strings.ToUpper(ucr.S)}, nil
 }
 
-func (stringdemo) Count(ctx context.Context, cr *api.CountRequest) (api.CountResponse, error) {
+func (stringdemo) Count(_ context.Context, cr *api.CountRequest) (api.CountResponse, error) {
 	return api.CountResponse{V: len(cr.S)}, nil
 }
 
-func (stringdemo) AddNumbers(ctx context.Context, arg1 int, arg2 int) (api.AddNumbersResponse, error) {
+func (stringdemo) AddNumbers(_ context.Context, arg1 int, arg2 int) (api.AddNumbersResponse, error) {
 	return api.AddNumbersResponse{Sum: arg1 + arg2}, nil
 }
